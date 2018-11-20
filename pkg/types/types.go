0e8a0f8b0fbc3374725a247a64dbdb268a72c98c
sam.sun <asdfsx@gmail.com>
2018-11-20 11:35:13 +0800

    update image version

---
+++ pkg/types/types.go
@@ -0,0 +1,18 @@
+package types
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/intel/multus-cni/types"
+)
+
+type NetworkAttachmentDefinitionList struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard list metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#types-kinds
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+
+	// Items is the list of Namespace objects in the list.
+	// More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/
+	Items []types.NetworkAttachmentDefinition `json:"items" protobuf:"bytes,2,rep,name=items"`
+}
